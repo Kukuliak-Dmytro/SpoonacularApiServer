5ab4b826a6f3fe11541ce83ba57e1a10ffad6b1a
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-05 12:49:07 +0300

    Successfull request

--- sp_server.js
+++ sp_server.js
@@ -3,21 +3,22 @@ const axios = require('axios');
 require('dotenv').config();  // To use environment variables
 
 const app = express();
-const PORT = 5000;  // You can change the port if needed
+const PORT = 5000; 
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Example route that makes an API call
-app.get('/',  (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const response =  axios.get('https://api.spoonacular.com/recipes/complexSearch', {
+    const response = await axios.get('https://api.spoonacular.com/recipes/complexSearch?number=5', {
       headers: {
         'x-api-key': `${process.env.apikey}` 
       }
     });
     res.writeHead(200);
-    res.write('Successfull connection')    
+    res.write(`${response.data}`)
+    console.log(response.data);    
 
   } catch (error) {
     console.error('Error fetching data:', error);
