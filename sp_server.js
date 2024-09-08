74de98da478d60ba3d9942a57fba1cc96f5dae0c
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-09-08 15:17:55 +0300

    Finally tinkering the fetching

--- sp_server.js
+++ sp_server.js
@@ -1,33 +1,33 @@
 const express = require('express');
-const cors = require('cors');  // Import the cors middleware
-const axios = require('axios');
-require('dotenv').config();  // To use environment variables
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000; 
+const PORT = 5000;
 
-// Middleware to parse JSON requests
 app.use(express.json());
 app.use(cors());
-// Example route that makes an API call
-app.get('/', async (req, res) => {
-  const path = req.query.path;  
 
-  try {
-    const response = await axios.get(`https://api.spoonacular.com/${path}`, {
-      headers: {
-        'x-api-key': `${process.env.apikey}` 
-      }
-    });
-    res.status(response.status).json(response.data);
-
-  } catch (error) {
+app.get('/', (req, res) => {
+  const apiURL = decodeURIComponent(req.query.path);
+  console.log(apiURL)
+  
+  fetch(apiURL, {
+    method: 'GET',
+    headers: {
+      'x-api-key': `${process.env.apikey}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
     console.error('Error fetching data:', error);
     res.status(500).json({ message: 'Server error' });
-  }
+  });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
